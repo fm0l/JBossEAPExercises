f9bf00e61882f2570ce6e71ef205781b5da9e41c
root <root@jboss.casoncello.local>
2018-01-09 12:08:55 +0100

    added sources

---
+++ sources/messaging/src/main/java/org/jboss/as/quickstarts/jms/HelloWorldMessaging.java
@@ -0,0 +1,120 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.quickstarts.jms;
+
+import java.util.logging.Logger;
+import java.util.Properties;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+
+@WebServlet("/HelloWorldMessaging")
+public class HelloWorldMessaging extends HttpServlet {
+    private static final Logger log = Logger.getLogger(HelloWorldMessaging.class.toString());
+
+    // Set up all the default values
+    private static final String DEFAULT_MESSAGE = "Hello, World!";
+    private static final String DEFAULT_CONNECTION_FACTORY = "jms/RemoteConnectionFactory";
+    private static final String DEFAULT_DESTINATION = "jms/queue/HELLOWORLDQueue";
+    private static final String DEFAULT_MESSAGE_COUNT = "1";
+    private static final String DEFAULT_USERNAME = "jmsadmin";
+    private static final String DEFAULT_PASSWORD = "jmsadmin";
+    private static final String INITIAL_CONTEXT_FACTORY = "org.wildfly.naming.client.WildFlyInitialContextFactory";
+    private static final String PROVIDER_URL = "http-remoting://192.168.122.98:8080";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.write("<h1>Quickstart: Example demonstrates the use of <strong>JMS 2.0</strong> and <strong>EJB 3.2 Message-Driven Bean</strong> in JBoss EAP.</h1>");
+
+        Context namingContext = null;
+
+        try {
+            String userName = System.getProperty("username", DEFAULT_USERNAME);
+            String password = System.getProperty("password", DEFAULT_PASSWORD);
+
+            // Set up the namingContext for the JNDI lookup
+            final Properties env = new Properties();
+            env.put(Context.INITIAL_CONTEXT_FACTORY, INITIAL_CONTEXT_FACTORY);
+            env.put(Context.PROVIDER_URL, System.getProperty(Context.PROVIDER_URL, PROVIDER_URL));
+            env.put(Context.SECURITY_PRINCIPAL, userName);
+            env.put(Context.SECURITY_CREDENTIALS, password);
+            namingContext = new InitialContext(env);
+
+            // Perform the JNDI lookups
+            String connectionFactoryString = System.getProperty("connection.factory", DEFAULT_CONNECTION_FACTORY);
+            log.info("Attempting to acquire connection factory \"" + connectionFactoryString + "\"");
+            ConnectionFactory connectionFactory = (ConnectionFactory) namingContext.lookup(connectionFactoryString);
+            log.info("Found connection factory \"" + connectionFactoryString + "\" in JNDI");
+
+            String destinationString = System.getProperty("destination", DEFAULT_DESTINATION);
+            log.info("Attempting to acquire destination \"" + destinationString + "\"");
+            Destination destination = (Destination) namingContext.lookup(destinationString);
+            log.info("Found destination \"" + destinationString + "\" in JNDI");
+
+            int count = Integer.parseInt(System.getProperty("message.count", DEFAULT_MESSAGE_COUNT));
+            String content = System.getProperty("message.content", DEFAULT_MESSAGE);
+
+            try (JMSContext context = connectionFactory.createContext(userName, password)) {
+                log.info("Sending " + count + " messages with content: " + content);
+                // Send the specified number of messages
+                for (int i = 0; i < count; i++) {
+                    context.createProducer().send(destination, content);
+                }
+
+                // Create the JMS consumer
+                JMSConsumer consumer = context.createConsumer(destination);
+                // Then receive the same number of messages that were sent
+                for (int i = 0; i < count; i++) {
+                    String text = consumer.receiveBody(String.class, 5000);
+                    log.info("Received message with content " + text);
+                }
+            }
+        } catch (NamingException e) {
+            log.severe(e.getMessage());
+        } finally {
+            if (namingContext != null) {
+                try {
+                    namingContext.close();
+                } catch (NamingException e) {
+                    log.severe(e.getMessage());
+                }
+            }
+        }
+  }
+  
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+  }
+}
